70877c54dc04a929b51d10359a7ec60d9ca3a02e
Santhosh Velaga <tosh@santhoshs-air.lan>
2021-02-18 21:16:52 -0800

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// https://codepen.io/RajRajeshDn/pen/LYPeBOV
+// https://dev.to/llorentegerman/building-a-ui-from-scratch-responsive-sidebar-and-header-443g
+
+function Navbar() {
+    return (
+        <div>
+            <p>Navbar goes here</p>
+        </div>
+    )
+}
+
+export default Navbar
