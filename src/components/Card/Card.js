929fc00a8e5728084e3af299000dd54867c7a3e5
Santhosh Velaga <toshvelaga@gmail.com>
2021-03-08 13:45:40 -0800

    added card styling

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="card-styles">
+      <p>Card Title</p>
+    </div>
+  );
+}
+
+export default Card;
--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Card from "../../components/Card/Card";
 import "./Dashboard.css";
 
 function Dashboard() {
@@ -11,7 +12,7 @@ function Dashboard() {
         <h2>
           The navigation bar will stay at the top of the page while scrolling
         </h2>
-
+        <Card />
         <p>Some text some text some text some text..</p>
         <p>Some text some text some text some text..</p>
         <p>Some text some text some text some text..</p>
