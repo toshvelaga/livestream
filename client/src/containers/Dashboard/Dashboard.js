d522a1c92da34895f598e2efdb22593846e9b1ac
Tosh Velaga <toshvelaga@gmail.com>
2021-07-10 22:03:00 -0500

    add: video ref in dashboard.js

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -1,18 +1,37 @@
-import React, { useState, useEffect} from "react";
-import Navbar from "../../components/Navbar/Navbar";
+import React, { useState, useEffect, useRef } from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 // import Card from "../../components/Card/Card";
-import "./Dashboard.css";
+import './Dashboard.css'
 
 function Dashboard() {
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    getVideo()
+  }, [videoRef])
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then((stream) => {
+        let video = videoRef.current
+        video.srcObject = stream
+        video.play()
+      })
+      .catch((err) => {
+        console.error('error:', err)
+      })
+  }
+
   return (
     <>
       <Navbar />
-      <div style={{ marginTop: "5rem" }} class="main">
+      <div style={{ marginTop: '5rem' }} class='main'>
         <h2>Scroll this page to see the effect</h2>
         <h2>
           The navigation bar will stay at the top of the page while scrolling
-        </h2>        
-        <video muted id="video" autoplay></video>
+        </h2>
+        <video muted id='video' autoplay ref={videoRef} />
         <button>start recording</button>
         <button>stop recording</button>
         {/* <Card />
@@ -21,7 +40,7 @@ function Dashboard() {
         <p>Some text some text some text some text..</p>
       </div>
     </>
-  );
+  )
 }
 
-export default Dashboard;
+export default Dashboard
