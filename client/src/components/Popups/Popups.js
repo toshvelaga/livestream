b7c5631f0a048e2764ef1051b40b9cbc90fe3e69
Tosh Velaga <toshvelaga@gmail.com>
2021-07-13 16:09:30 -0500

    encodeURIComponent in Popup

--- client/src/components/Popups/Popups.js
+++ client/src/components/Popups/Popups.js
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Popups() {
+  const CLIENT_ID = encodeURIComponent(process.env.REACT_APP_TWITCH_CLIENT_ID)
+  const REDIRECT_URI = encodeURIComponent(
+    'https://mjefmmgpdalfommpehabccnhodbppfje.chromiumapp.org/'
+  )
+  const RESPONSE_TYPE = encodeURIComponent('token id_token')
+  const SCOPE = encodeURIComponent('openid')
+
+  let redirect_uri = 'http://localhost:3000/'
   let url = `https://id.twitch.tv/oauth2/authorize
     ?client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}
-    &redirect_uri=<your registered redirect URI>
+    &redirect_uri=${redirect_uri}
     &response_type=<type>
-    &scope=<space-separated list of scopes></space-separated>`
+    &scope=channel:read:stream_key`
 
   let url2 = `https://jsonplaceholder.typicode.com/todos/1`
 
   const twitchAuthUser = async () => {
     try {
-      const response = await axios.get(url2)
+      const response = await axios.get(url)
       console.log(response)
     } catch (error) {
       console.error(error)
