e5a28160329d70cbfa4f1ec1c4860c9b0c426a72
Tosh Velaga <toshvelaga@gmail.com>
2021-07-14 14:29:26 -0500

    add Code.js

---
+++ client/src/containers/Code/Code.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TextInput from '../../components/TextInput/TextInput'
+import Button from '../../components/Buttons/Button'
+
+function Code() {
+  return (
+    <div>
+      <TextInput />
+      <Button />
+    </div>
+  )
+}
+
+export default Code
