e449e1f6995a3a8fd95606901c3a879a868b614c
Tosh Velaga <toshvelaga@gmail.com>
2021-07-14 14:40:35 -0500

    add styles to Code.js

--- client/src/containers/Code/Code.js
+++ client/src/containers/Code/Code.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react'
 import TextInput from '../../components/TextInput/TextInput'
 import Button from '../../components/Buttons/Button'
+import './Code.css'
 
 function Code() {
   const [code, setCode] = useState('')
   return (
-    <div>
-      <TextInput
-        label='Code'
-        placeholder='Enter 6 digit code'
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-      />
-      <Button
-        fx={() => alert('clicked')}
-        style={{ width: '100%' }}
-        title='Log In'
-      />
-    </div>
+    <>
+      <div className='code-container'>
+        <TextInput
+          label='Code'
+          placeholder='Enter 6 digit code'
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <Button
+          fx={() => alert('clicked')}
+          style={{ width: '100%' }}
+          title='Log In'
+        />
+      </div>
+    </>
   )
 }
 
