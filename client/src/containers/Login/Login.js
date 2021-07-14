afd3bf496529e2af387aed1a3ba08faae54b78b4
Tosh Velaga <toshvelaga@gmail.com>
2021-07-14 13:53:03 -0500

    edit Login

--- client/src/containers/Login/Login.js
+++ client/src/containers/Login/Login.js
@@ -1,70 +1,47 @@
-import React, { useState } from "react";
-import TextInput from "../../components/TextInput/TextInput";
+import React, { useState } from 'react'
+import TextInput from '../../components/TextInput/TextInput'
 // import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
-import { Link } from "react-router-dom";
-import "./Login.css";
-import Button from "../../components/Buttons/Button";
+import { Link } from 'react-router-dom'
+import './Login.css'
+import Button from '../../components/Buttons/Button'
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   //   const [errorMsgEmail, seterrorMsgEmail] = useState("");
   //   const [errorMsgPassword, seterrorMsgPassword] = useState("");
 
   return (
     <>
-      <div className="login-container">
+      <div className='login-container'>
         <div>
           <TextInput
-            label="Email Address"
-            placeholder="Email Address"
+            label='Email Address'
+            placeholder='Email Address'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           {/* {errorMsgEmail ? <ErrorMessage errorMsg={errorMsgEmail} /> : null} */}
         </div>
-        <div>
-          <TextInput
-            label="Password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {/* {errorMsgPassword ? (
-            <ErrorMessage errorMsg={errorMsgPassword} />
-          ) : null} */}
-        </div>
+        <Button style={{ width: '100%' }} title='Sign In' />
 
-        <Button style={{ width: "100%" }} title="Sign In" />
-
-        <p>
-          <Link
-            style={{
-              textDecoration: "none",
-              color: "grey",
-            }}
-            to="/forgot-password"
-          >
-            Forgot your username or password?
-          </Link>
-        </p>
-        <div className="login-button">
+        <div className='login-button'>
           {/* <PrimaryButton style={{ width: "100%" }} title="Submit" fx={submit} /> */}
         </div>
-        <p style={{ color: "grey", marginTop: "1rem", textAlign: "center" }}>
+        <p style={{ color: 'grey', marginTop: '1rem', textAlign: 'center' }}>
           <Link
             style={{
-              textDecoration: "none",
-              color: "grey",
+              textDecoration: 'none',
+              color: 'grey',
             }}
-            to="/register"
+            to='/register'
           >
             Don't have an account? Sign Up
           </Link>
         </p>
       </div>
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
