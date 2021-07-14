197b8c73fbefdbe0872a0f4a21036b9cde2b4879
Tosh Velaga <toshvelaga@gmail.com>
2021-07-14 14:32:32 -0500

    add state to Code.js

--- client/src/containers/Code/Code.js
+++ client/src/containers/Code/Code.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextInput from '../../components/TextInput/TextInput'
 import Button from '../../components/Buttons/Button'
 
 function Code() {
+  const [code, setCode] = useState('')
   return (
     <div>
-      <TextInput />
+      <TextInput
+        label='Code'
+        placeholder='Enter 6 digit code'
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
       <Button />
     </div>
   )
