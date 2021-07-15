98346b250a2933c943aac3cd62a066d930cf44ad
Tosh Velaga <toshvelaga@gmail.com>
2021-07-14 20:46:18 -0500

    add to schema.sql

---
+++ server/schema.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users(
+    user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_email character varying(255) NOT NULL UNIQUE,
+    user_code integer,
+    user_date_created timestamp without time zone
+);
