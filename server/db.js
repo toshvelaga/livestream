0e201a75c257bd72ec125d959fcddbfbb8c82456
Tosh Velaga <toshvelaga@gmail.com>
2021-07-14 20:49:21 -0500

    add to db.js

---
+++ server/db.js
@@ -0,0 +1,26 @@
+// node-postgres doc: https://www.freecodecamp.org/news/fullstack-react-blog-app-with-express-and-psql/
+// deploy PERN app to Heroku: https://www.youtube.com/watch?v=ZJxUOOND5_A&t=138s&ab_channel=TheStoicProgrammers
+
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  post: process.env.PG_PORT,
+}
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}
+
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? proConfig : devConfig
+)
+
+module.exports = pool
