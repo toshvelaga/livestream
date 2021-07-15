c01149d43f33f25a0a382afd477b7bfa9d3e614e
Tosh Velaga <toshvelaga@gmail.com>
2021-07-15 16:29:31 -0500

    compareCode

---
+++ server/routes/compareCode.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const pool = require('../db')
+
+router.post('/compare-code', async (req, res) => {
+  let userEnteredCode = req.body.code
+  let user_id = req.body.user_id
+
+  let code = await pool.query(
+    `SELECT code FROM users WHERE users.user_id = $1`,
+    [user_id]
+  )
+
+  console.log(userEnteredCode && code)
+})
