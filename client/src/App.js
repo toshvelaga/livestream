5935d93f71c8507105b70a1d19e2bc8904688395
Tosh Velaga <toshvelaga@gmail.com>
2021-07-16 21:25:04 -0500

    App.js useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useEffect, useState } from 'react'
 import './App.css'
 import {
   BrowserRouter as Router,
@@ -8,39 +8,46 @@ import {
 } from 'react-router-dom'
 import Login from './containers/Login/Login'
 import Register from './containers/Register/Register'
-import ForgotPassword from './containers/ForgotPassword/ForgotPassword'
 import Popups from './components/Popups/Popups'
 import Code from './containers/Code/Code'
 
 const Dashboard = lazy(() => import('./containers/Dashboard/Dashboard'))
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState('')
+  useEffect(() => {
+    let login = getCookie('isLoggedIn')
+    setisLoggedIn(login)
+  }, [])
   return (
     <>
       <Router>
         {/* A <Switch> looks through its children <Routes> and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path='/dashboard'>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Dashboard />
-            </Suspense>
-          </Route>
-
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/register/code'>
-            <Code />
-          </Route>
-          <Route path='/register'>
-            <Register />
-          </Route>
-
-          <Route path='/'>
-            <Popups />
-          </Route>
-        </Switch>
+        {!isLoggedIn ? (
+          <Switch>
+            <Route path='/dashboard'>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Dashboard />
+              </Suspense>
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/register/code'>
+              <Code />
+            </Route>
+            <Route path='/register'>
+              <Register />
+            </Route>
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path='/'>
+              <Popups />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </>
   )
