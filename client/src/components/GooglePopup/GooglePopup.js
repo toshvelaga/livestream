b6207946f724a1759155fc04799a1867a52a5aa8
Tosh Velaga <toshvelaga@gmail.com>
2021-07-18 00:53:14 -0500

    Google Popup

--- client/src/components/GooglePopup/GooglePopup.js
+++ client/src/components/GooglePopup/GooglePopup.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function GooglePopup() {
-  return <div></div>
+  var client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID
+  var redirect = encodeURIComponent('http://localhost:3000/dashboard')
+  var scope = 'https://www.googleapis.com/auth/youtube'
+
+  const url = `https://accounts.google.com/o/oauth2/auth?
+    client_id=${client_id}&
+    redirect_uri=${redirect}&
+    scope=${scope}&
+    response_type=token`
+
+  return (
+    <div>
+      <a href={url}>
+        <button>Click me</button>
+      </a>
+    </div>
+  )
 }
 
 export default GooglePopup
