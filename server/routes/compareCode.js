ca6e2b22016924630df8712aa08caffe8e3ab18f
Tosh Velaga <toshvelaga@gmail.com>
2021-07-17 20:41:09 -0500

    compareCode update user_set_login

--- server/routes/compareCode.js
+++ server/routes/compareCode.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const pool = require('../db')
+const express = require('express'),
+  router = express.Router(),
+  pool = require('../db')
 
 router.post('/compare-code', async (req, res) => {
   let userEnteredCode = req.body.code
   let userId = req.body.userId
+  const date = new Date().toUTCString()
 
   let code = await pool.query(
     `SELECT user_code FROM users WHERE user_id = $1`,
     [userId]
+  )
+
+  pool.query(
+    `UPDATE users SET user_set_login = $1 WHERE user_id = $2`,
+    [date, userId],
+    (q_err, q_res) => {
+      res.json(q_res.rows)
+    }
   )
 
   console.log(code)
