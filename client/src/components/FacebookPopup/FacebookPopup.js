3769a039a0820378ecb3355015912a0dc03307c1
Tosh Velaga <toshvelaga@gmail.com>
2021-07-19 17:35:25 -0500

    Facebook popup

---
+++ client/src/components/FacebookPopup/FacebookPopup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FacebookPopup() {
+  return (
+    <div>
+      <canvas width='640' height='360'></canvas>
+      <nav>
+        <button data-action='goLive'>Go Live</button>
+      </nav>
+    </div>
+  )
+}
+
+export default FacebookPopup
