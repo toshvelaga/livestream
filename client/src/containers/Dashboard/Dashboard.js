5542dd1e26586d1729917a3f5dc5b64515265dcd
Tosh Velaga <toshvelaga@gmail.com>
2021-07-23 22:01:51 -0500

    refactor Dashboard and add button for mute

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -52,38 +52,39 @@ function Dashboard() {
     }
   }, [])
 
-  useEffect(() => {
-    const ws = new WebSocket(
-      window.location.protocol.replace('http', 'ws') +
-        '//' + // http: -> ws:, https: -> wss:
-        'localhost:3000'
-    )
-    console.log(ws)
-    let mediaStream
-    let mediaRecorder
-    ws.addEventListener('open', (e) => {
-      console.log('WebSocket Open', e)
-      mediaStream = document.querySelector('video').captureStream(30) // 30 FPS
-      mediaRecorder = new MediaRecorder(mediaStream, {
-        mimeType: 'video/webm;codecs=h264',
-        videoBitsPerSecond: 3 * 1024 * 1024,
-      })
+  // useEffect(() => {
+  //   const ws = new WebSocket(
+  //     window.location.protocol.replace('http', 'ws') +
+  //       '//' + // http: -> ws:, https: -> wss:
+  //       'localhost:3000'
+  //   )
+  //   console.log(ws)
+  //   let mediaStream
+  //   let mediaRecorder
+  //   ws.addEventListener('open', (e) => {
+  //     console.log('WebSocket Open', e)
+  //     mediaStream = document.querySelector('video').captureStream(30) // 30 FPS
+  //     mediaRecorder = new MediaRecorder(mediaStream, {
+  //       mimeType: 'video/webm;codecs=h264',
+  //       videoBitsPerSecond: 3 * 1024 * 1024,
+  //     })
 
-      mediaRecorder.addEventListener('dataavailable', (e) => {
-        ws.send(e.data)
-        console.log('send data')
-      })
+  //     mediaRecorder.addEventListener('dataavailable', (e) => {
+  //       ws.send(e.data)
+  //       console.log('send data')
+  //     })
 
-      // mediaRecorder.addEventListener('stop', ws.close.bind(ws))
+  //     // mediaRecorder.addEventListener('stop', ws.close.bind(ws))
 
-      mediaRecorder.start(1000) // Start recording, and dump data every second
-    })
+  //     mediaRecorder.start(1000)
+  //     // Start recording, and dump data every second
+  //   })
 
-    ws.addEventListener('close', (e) => {
-      console.log('WebSocket Close', e)
-      // mediaRecorder.stop()
-    })
-  }, [])
+  //   ws.addEventListener('close', (e) => {
+  //     console.log('WebSocket Close', e)
+  //     // mediaRecorder.stop()
+  //   })
+  // }, [])
 
   function calculateSize(srcSize, dstSize) {
     var srcRatio = srcSize.width / srcSize.height
@@ -112,6 +113,7 @@ function Dashboard() {
         <div className='button-container'>
           <button>Go Live</button>
           <button>Stop Recording</button>
+          <button>Mute</button>
         </div>
       </div>
     </>
