c405d47dd250f15dab683827fb8ab2e141252f7f
Tosh Velaga <toshvelaga@gmail.com>
2021-07-24 23:21:20 -0500

    add useUserMedia custom hook

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -2,57 +2,22 @@ import React, { useState, useEffect, useRef } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './Dashboard.css'
 
+const CAPTURE_OPTIONS = {
+  audio: false,
+  video: { facingMode: 'environment' },
+}
+
 function Dashboard() {
-  const CAPTURE_OPTIONS = {
-    audio: false,
-    video: { facingMode: 'environment' },
+  const videoRef = useRef()
+  const mediaStream = useUserMedia(CAPTURE_OPTIONS)
+
+  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+    videoRef.current.srcObject = mediaStream
   }
 
-  useEffect(() => {
-    var video = document.getElementById('video')
-
-    if (navigator.mediaDevices.getUserMedia) {
-      var successCallback = function (stream) {
-        video.srcObject = stream
-      }
-      var errorCallback = function (error) {
-        console.log(error)
-      }
-      navigator.mediaDevices
-        .getUserMedia(CAPTURE_OPTIONS)
-        .then(successCallback, errorCallback)
-    }
-
-    const canvas = document.getElementById('canvas')
-    const context = canvas.getContext('2d')
-
-    var ctx = canvas.getContext('2d')
-    ctx.moveTo(0, 0)
-    ctx.lineTo(200, 100)
-    ctx.stroke()
-
-    function renderFrame() {
-      // re-register callback
-      requestAnimationFrame(renderFrame)
-      // set internal canvas size to match HTML element size
-      canvas.width = canvas.scrollWidth
-      canvas.height = canvas.scrollHeight
-      if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        // scale and horizontally center the camera image
-        var videoSize = { width: video.videoWidth, height: video.videoHeight }
-        var canvasSize = { width: canvas.width, height: canvas.height }
-        var renderSize = calculateSize(videoSize, canvasSize)
-        var xOffset = (canvasSize.width - renderSize.width) / 2
-        context.drawImage(
-          video,
-          xOffset,
-          0,
-          renderSize.width,
-          renderSize.height
-        )
-      }
-    }
-  }, [])
+  const handleCanPlay = () => {
+    videoRef.current.play()
+  }
 
   // useEffect(() => {
   //   const ws = new WebSocket(
@@ -88,22 +53,6 @@ function Dashboard() {
   //   })
   // }, [])
 
-  function calculateSize(srcSize, dstSize) {
-    var srcRatio = srcSize.width / srcSize.height
-    var dstRatio = dstSize.width / dstSize.height
-    if (dstRatio > srcRatio) {
-      return {
-        width: dstSize.height * srcRatio,
-        height: dstSize.height,
-      }
-    } else {
-      return {
-        width: dstSize.width,
-        height: dstSize.width / srcRatio,
-      }
-    }
-  }
-
   return (
     <>
       <Navbar />
@@ -120,6 +69,33 @@ function Dashboard() {
       </div>
     </>
   )
+}
+
+const useUserMedia = (requestedMedia) => {
+  const [mediaStream, setMediaStream] = useState(null)
+
+  useEffect(() => {
+    async function enableStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(requestedMedia)
+        setMediaStream(stream)
+      } catch (err) {
+        // Removed for brevity
+      }
+    }
+
+    if (!mediaStream) {
+      enableStream()
+    } else {
+      return function cleanup() {
+        mediaStream.getTracks().forEach((track) => {
+          track.stop()
+        })
+      }
+    }
+  }, [mediaStream, requestedMedia])
+
+  return mediaStream
 }
 
 export default Dashboard
