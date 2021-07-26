128aa22a5309e9d166c7573233e3f70fbc7e0113
Tosh Velaga <toshvelaga@gmail.com>
2021-07-26 00:05:55 -0500

    stream to youtube and twitch with audio and video using ffmpeg

--- server/server.js
+++ server/server.js
@@ -57,20 +57,75 @@ wss.on('connection', (ws, req) => {
     // 'lavfi',
     // '-i',
     // 'anullsrc',
-
     '-i',
     '-',
 
+    // video codec config: low latency, adaptive bitrate
+    '-c:v',
+    'libx264',
+    '-preset',
+    'veryfast',
+    '-tune',
+    'zerolatency',
+
+    // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+    '-c:a',
+    'aac',
+    '-strict',
+    '-2',
+    '-ar',
+    '44100',
+    '-b:a',
+    '64k',
+
+    //force to overwrite
+    '-y',
+
+    // used for audio sync
+    '-use_wallclock_as_timestamps',
+    '1',
+    '-async',
+    '1',
+
+    //'-filter_complex', 'aresample=44100', // resample audio to 44100Hz, needed if input is not 44100
+    //'-strict', 'experimental',
+    '-bufsize',
+    '1000',
     '-f',
     'flv',
-    '-c',
-    'copy',
     `${process.env.TWITCH_STREAM_ADDRESS}`,
+
+    // video codec config: low latency, adaptive bitrate
+    '-c:v',
+    'libx264',
+    '-preset',
+    'veryfast',
+    '-tune',
+    'zerolatency',
+
+    // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+    '-c:a',
+    'aac',
+    '-strict',
+    '-2',
+    '-ar',
+    '44100',
+    '-b:a',
+    '64k',
+
+    //force to overwrite
+    '-y',
+
+    // used for audio sync
+    '-use_wallclock_as_timestamps',
+    '1',
+    '-async',
+    '1',
+
     '-f',
     'flv',
-    '-c',
-    'copy',
     `${process.env.YOUTUBE_STREAM_ADDRESS}`,
+
     // '-f',
     // 'flv',
     // '-c',
