eeb1d151a113a2a7eea6a5a7b165f76830843ede
Tosh Velaga <toshvelaga@gmail.com>
2021-07-27 15:40:05 -0500

    add user_last_login to pg table users

--- server/schema.sql
+++ server/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE users(
     user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     user_email character varying(255) NOT NULL UNIQUE,
     user_code integer,
-    user_date_created timestamp without time zone
+    user_date_created timestamp without time zone,
+    user_last_login timestamp without time zone
 );
