419ad083dadf4e1d9ad8ed6d0de5a40353c359c5
Tosh Velaga <toshvelaga@gmail.com>
2021-07-29 21:06:58 -0500

    add: PageNotFound container

---
+++ client/src/containers/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PageNotFound.css'
+
+function PageNotFound() {
+  return (
+    <div>
+      <h2 className='page-not-found-text'>Page Not Found</h2>
+    </div>
+  )
+}
+
+export default PageNotFound
