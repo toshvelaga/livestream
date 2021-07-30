9771bafeb450bfcc7d4f5dc5193c587309e33ca3
Tosh Velaga <toshvelaga@gmail.com>
2021-07-29 22:48:57 -0500

    add AuthRoute

---
+++ client/src/AuthRoute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import getCookie from './utils/getCookie'
+
+function AuthRoute({ component: Component, ...rest }) {
+  const isLoggedIn = getCookie('isLoggedIn')
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!isLoggedIn) {
+          return <Component {...props} />
+        } else {
+          return (
+            <Redirect
+              to={{ pathname: '/dashboard', state: { from: props.location } }}
+            />
+          )
+        }
+      }}
+    />
+  )
+}
+
+export default AuthRoute
