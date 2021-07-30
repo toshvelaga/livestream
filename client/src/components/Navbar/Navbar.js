994f99a0e42d7ea981a6fcbf322a05bb62849f6a
Tosh Velaga <toshvelaga@gmail.com>
2021-07-29 23:06:18 -0500

    comment out top nav

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,57 +1,58 @@
-import React, { useState, useLayoutEffect } from "react";
-import * as FaIcons from "react-icons/fa";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-import SideNavbar from "./SideNavbar";
-import { SidebarData } from "./SidebarData";
-import { IconContext } from "react-icons";
+import React, { useState, useLayoutEffect } from 'react'
+import * as FaIcons from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import SideNavbar from './SideNavbar'
+import { SidebarData } from './SidebarData'
+import { IconContext } from 'react-icons'
 
 function Navbar() {
-  const [sidebar, setSidebar] = useState(false);
-  const [width, setWidth] = useState(0);
+  const [sidebar, setSidebar] = useState(false)
+  const [width, setWidth] = useState(0)
 
   const showSidebar = () => {
-    setSidebar(!sidebar);
-  };
+    setSidebar(!sidebar)
+  }
 
   useLayoutEffect(() => {
     const updateWindowDimensions = () => {
-      const newWidth = window.innerWidth;
-      setWidth(newWidth);
-      console.log("updating width");
-    };
+      const newWidth = window.innerWidth
+      setWidth(newWidth)
+      console.log('updating width')
+    }
 
-    window.addEventListener("resize", updateWindowDimensions);
+    window.addEventListener('resize', updateWindowDimensions)
 
-    return () => window.removeEventListener("resize", updateWindowDimensions);
-  }, []);
+    return () => window.removeEventListener('resize', updateWindowDimensions)
+  }, [])
 
   const closeSideNav = () => {
     if (sidebar === true && width > 555) {
-      setSidebar(false);
+      setSidebar(false)
     }
-  };
+  }
 
   return (
     <>
       {/* sticky top navbar */}
       {sidebar
-        ? (document.body.style = "background: grey")
-        : (document.body.style = "background: #fff")}
+        ? (document.body.style = 'background: grey')
+        : (document.body.style = 'background: #fff')}
       <div
         style={
-          sidebar ? { backgroundColor: "grey" } : { backgroundColor: "#fff" }
+          sidebar ? { backgroundColor: 'grey' } : { backgroundColor: '#fff' }
         }
-        id="top-navbar"
+        id='top-navbar'
       >
-        <span className="user-circle-icon">
+        {/* <span className="user-circle-icon">
           <FaIcons.FaUserCircle size={30} />
         </span>
         <span className="notification-icon">
           <FaIcons.FaBell size={20} />
           {closeSideNav()}
-        </span>
-        <span onClick={showSidebar} className="hamburger-icon">
+        </span> */}
+
+        <span onClick={showSidebar} className='hamburger-icon'>
           <FaIcons.FaBars size={20} />
         </span>
       </div>
@@ -59,11 +60,11 @@ function Navbar() {
       <SideNavbar />
       {/* Mobile navbar overlay for small screen size */}
 
-      <IconContext.Provider value={{ color: "black" }}>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link to="#" className="hamburger-icon">
+      <IconContext.Provider value={{ color: 'black' }}>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='hamburger-icon'>
                 <FaIcons.FaBars size={20} />
               </Link>
             </li>
@@ -72,16 +73,16 @@ function Navbar() {
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
                     {item.icon}
-                    <span style={{ marginLeft: "5px" }}>{item.title}</span>
+                    <span style={{ marginLeft: '5px' }}>{item.title}</span>
                   </Link>
                 </li>
-              );
+              )
             })}
           </ul>
         </nav>
       </IconContext.Provider>
     </>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
