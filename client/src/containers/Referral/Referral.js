04fc6f461ea3898384fd52c73fbbf654c2936007
Tosh Velaga <toshvelaga@gmail.com>
2021-07-30 17:59:47 -0500

    add referral container

--- client/src/containers/Referral/Referral.js
+++ client/src/containers/Referral/Referral.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import TextInput from '../../components/TextInput/TextInput'
+import Button from '../../components/Button/Button'
 import './Referral.css'
 
 function Referral() {
+  const [referralEmail, setReferralEmail] = useState('')
+  const handleClick = () => {
+    console.log('click')
+  }
   return (
     <>
       <Navbar />
-      <h2>Referral</h2>
+      <div className='referrals-container'>
+        <h2>Referrals</h2>
+        <TextInput
+          label='Refer a friend'
+          placeholder=''
+          value={referralEmail}
+          onChange={(e) => setReferralEmail(e.target.value)}
+          errorMsg={null}
+        />
+        <Button style={{ width: '100%' }} title='Submit' fx={handleClick} />
+      </div>
     </>
   )
 }
