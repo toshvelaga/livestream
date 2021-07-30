03cc5051cf861ea0e7dbc1b4366ef6fc37423178
Tosh Velaga <toshvelaga@gmail.com>
2021-07-30 12:43:32 -0500

    add state for stream keys in Destinations

--- client/src/containers/Destinations/Destinations.js
+++ client/src/containers/Destinations/Destinations.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import TextInput from '../../components/TextInput/TextInput'
 import './Destinations.css'
 
 function Destinations() {
+  const [twitchStreamKey, setTwitchStreamKey] = useState('')
+  const [youtubeStreamKey, setYoutubeStreamKey] = useState('')
+  const [facebookStreamKey, setFacebookStreamKey] = useState('')
+
   return (
     <>
       <Navbar />
-      <h2>Destinations page</h2>
+      <div className='destinations-container'>
+        <h2>Destinations page</h2>
+        <TextInput
+          label='Twitch Stream Key'
+          placeholder=''
+          value={twitchStreamKey}
+          onChange={(e) => setTwitchStreamKey(e.target.value)}
+          errorMsg={null}
+        />
+        <TextInput
+          label='Youtube Stream Key'
+          placeholder=''
+          value={youtubeStreamKey}
+          onChange={(e) => setYoutubeStreamKey(e.target.value)}
+          errorMsg={null}
+        />
+        <TextInput
+          label='Facebook Stream Key'
+          placeholder=''
+          value={facebookStreamKey}
+          onChange={(e) => setFacebookStreamKey(e.target.value)}
+          errorMsg={null}
+        />
+      </div>
     </>
   )
 }
