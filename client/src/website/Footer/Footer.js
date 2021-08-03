45755df5d1e098f18b8af0578b95bd70b38c1e02
Tosh Velaga <toshvelaga@gmail.com>
2021-08-02 22:57:59 -0500

    change styles for website

--- client/src/website/Footer/Footer.js
+++ client/src/website/Footer/Footer.js
@@ -1,26 +1,23 @@
-import React from "react";
-import "./Footer.css";
-import { Link } from "react-router-dom";
+import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom'
 
 function Footer() {
-	return (
-		<div className="bottom-footer">
-			<span>Made in the United States of America 🇺🇸</span>
-			<span className="footer-link">
-				<Link style={{ textDecoration: "none", color: "#fff" }} to="/terms">
-					Terms of Service
-				</Link>
-			</span>
-			<span className="footer-link">
-				<Link
-					style={{ textDecoration: "none", color: "#fff" }}
-					to="privacy-policy"
-				>
-					Privacy Policy
-				</Link>
-			</span>
-		</div>
-	);
+  return (
+    <div className='bottom-footer'>
+      <span>Made in the United States of America 🇺🇸</span>
+      <span className='footer-link'>
+        <Link style={{ textDecoration: 'none' }} to='/terms'>
+          Terms of Service
+        </Link>
+      </span>
+      <span className='footer-link'>
+        <Link style={{ textDecoration: 'none' }} to='privacy-policy'>
+          Privacy Policy
+        </Link>
+      </span>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
