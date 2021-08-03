76bfbebd8fe025cfcbbfdc44b07ca26ad0f923d4
Tosh Velaga <toshvelaga@gmail.com>
2021-08-03 13:06:53 -0500

    update content in Accordion on website

--- client/src/website/Collapsible/Accordion.js
+++ client/src/website/Collapsible/Accordion.js
@@ -1,58 +1,59 @@
-import React, { Component } from "react";
-import Panel from "./Panel";
-import "./Accordion.css";
+import React, { Component } from 'react'
+import Panel from './Panel'
+import './Accordion.css'
 
 // https://codepen.io/DNLHC/pen/BRmJrj
 
 export default class Accordion extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props)
 
-		this.state = {
-			activeTab: 0,
-		};
+    this.state = {
+      activeTab: 0,
+    }
 
-		this.activateTab = this.activateTab.bind(this);
-	}
+    this.activateTab = this.activateTab.bind(this)
+  }
 
-	panels = [
-		{
-			label: "How many episodes can I host on the free plan?",
-			content:
-				"All users who sign up now will be able to upload as many episodes as they want. Even in the future if there are price changes you will be able to still upload as many as you like without having to pay.",
-		},
-		{
-			label: "Can I get a discount?",
-			content:
-				"Possibly! Please send us a message and we can discuss your case.",
-		},
-		{
-			label: "Can I share my account with my co-host?",
-			content: "Yes. We are working on adding contributors.",
-		},
-	];
+  panels = [
+    {
+      label: 'What features are you adding in the future?',
+      content:
+        'Authentication via twitch, google and facebook instead of having to find your stream key. The ability to stream with other users using webRTC. Ability to add overlays on your content in real time',
+    },
+    {
+      label: 'How much does this cost?',
+      content:
+        "Right now it's totally free! Will be adding paid plans with advanced features in the future.",
+    },
+    {
+      label: 'Where can I find the source code?',
+      content:
+        'All of the source code is available on GitHub: https://github.com/toshvelaga/livestream',
+    },
+  ]
 
-	activateTab(index) {
-		this.setState((prev) => ({
-			activeTab: prev.activeTab === index ? -1 : index,
-		}));
-	}
+  activateTab(index) {
+    this.setState((prev) => ({
+      activeTab: prev.activeTab === index ? -1 : index,
+    }))
+  }
 
-	render() {
-		// const { panels } = this.props;
-		const { activeTab } = this.state;
-		return (
-			<div className="accordion" role="tablist">
-				{this.panels.map((panel, index) => (
-					<Panel
-						key={index}
-						activeTab={activeTab}
-						index={index}
-						{...panel}
-						activateTab={this.activateTab.bind(null, index)}
-					/>
-				))}
-			</div>
-		);
-	}
+  render() {
+    // const { panels } = this.props;
+    const { activeTab } = this.state
+    return (
+      <div className='accordion' role='tablist'>
+        {this.panels.map((panel, index) => (
+          <Panel
+            key={index}
+            activeTab={activeTab}
+            index={index}
+            {...panel}
+            activateTab={this.activateTab.bind(null, index)}
+          />
+        ))}
+      </div>
+    )
+  }
 }
