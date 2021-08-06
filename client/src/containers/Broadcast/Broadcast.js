38f53a961726c133c3c8795fad9b961236b67b62
Tosh Velaga <toshvelaga@gmail.com>
2021-08-06 17:17:44 -0500

    change from ws to wss for prod

--- client/src/containers/Broadcast/Broadcast.js
+++ client/src/containers/Broadcast/Broadcast.js
@@ -77,7 +77,7 @@ function Broadcast() {
     ws.current =
       process.env.NODE_ENV === 'production'
         ? new WebSocket(
-            'ws://ohmystream.co' +
+            'wss://ohmystream.co' +
               `?twitchStreamKey=${twitchStreamKey}&youtubeStreamKey=${youtubeStreamKey}&facebookStreamKey=${facebookStreamKey}`
           )
         : new WebSocket(
