70b9ece7f33696c86d9c9e3c88c06c225da68ebe
Tosh Velaga <toshvelaga@gmail.com>
2021-08-06 22:37:20 -0500

    update wss url in Broadcast

--- client/src/containers/Broadcast/Broadcast.js
+++ client/src/containers/Broadcast/Broadcast.js
@@ -77,8 +77,7 @@ function Broadcast() {
     ws.current =
       process.env.NODE_ENV === 'production'
         ? new WebSocket(
-            'wss://' +
-              window.location.host +
+            'wss://ohmystream.com:3001' +
               `?twitchStreamKey=${twitchStreamKey}&youtubeStreamKey=${youtubeStreamKey}&facebookStreamKey=${facebookStreamKey}`
           )
         : new WebSocket(
