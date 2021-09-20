e43e18258f51efbba58801055b7c7b47763f8a3c
Tosh Velaga <toshvelaga@gmail.com>
2021-09-19 21:28:59 -0700

    api.js fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'ohmystream.xyz/api'
+    ? 'http://www.ohmystream.xyz/api'
     : 'http://localhost:5001/api'
 
 const API = axios.create({
