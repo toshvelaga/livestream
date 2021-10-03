e66971bbd56343ad92ba2888d1d3a40ee696b990
Tosh Velaga <toshvelaga@gmail.com>
2021-10-02 22:19:36 -0600

    add https to api.js for prod

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://www.ohmystream.xyz/api'
+    ? 'https://www.ohmystream.xyz/api'
     : 'http://localhost:5001/api'
 
 const API = axios.create({
