8ec341ce558682fcd8a59d11b5c4b896c1a519a3
Tosh Velaga <toshvelaga@gmail.com>
2021-10-08 17:57:58 -0600

    page not found edited

--- client/src/containers/PageNotFound/PageNotFound.js
+++ client/src/containers/PageNotFound/PageNotFound.js
@@ -4,7 +4,9 @@ import './PageNotFound.css'
 function PageNotFound() {
   return (
     <div>
-      <h2 className='page-not-found-text'>Page Not Found</h2>
+      <h2 className='page-not-found-text'>
+        Whoops...sorry friend this page was not found!
+      </h2>
     </div>
   )
 }
