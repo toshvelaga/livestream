d009467a0f685957c4dab4f6f672748d3961a50f
Tosh Velaga <toshvelaga@gmail.com>
2021-10-08 20:04:27 -0600

    convert functions to es6

--- client/src/containers/Destinations/Destinations.js
+++ client/src/containers/Destinations/Destinations.js
@@ -52,28 +52,21 @@ function Destinations() {
     return gapi.auth2
       .getAuthInstance()
       .signIn({ scope: 'https://www.googleapis.com/auth/youtube.force-ssl' })
-      .then(
-        function () {
-          console.log('Sign-in successful')
-        },
-        function (err) {
-          console.error('Error signing in', err)
-        }
-      )
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => console.log(err))
   }
 
   const loadClient = () => {
     gapi.client.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY)
     return gapi.client
       .load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest')
-      .then(
-        function () {
-          console.log('GAPI client loaded for API')
-        },
-        function (err) {
-          console.error('Error loading GAPI client for API', err)
-        }
-      )
+      .then((res) => {
+        console.log('GAPI client loaded for API')
+        console.log(res)
+      })
+      .catch((err) => console.log('Error loading GAPI client for API', err))
   }
   // Make sure the client is loaded and sign-in is complete before calling this method.
   const execute = () => {
@@ -97,15 +90,13 @@ function Destinations() {
           },
         },
       })
-      .then(
-        function (response) {
-          // Handle the results here (response.result has the parsed body).
-          console.log('Response', response)
-        },
-        function (err) {
-          console.error('Execute error', err)
-        }
-      )
+      .then((res) => {
+        // Handle the results here (response.result has the parsed body).
+        console.log('Response', res)
+      })
+      .catch((err) => {
+        console.log('Execute error', err)
+      })
   }
 
   gapi.load('client:auth2', function () {
