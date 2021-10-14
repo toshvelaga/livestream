76ed25075a35a9c939dd5c0c303ef4138fa3a724
Tosh Velaga <toshvelaga@gmail.com>
2021-10-13 22:46:57 -0600

    add modal

--- client/src/containers/Broadcast/Broadcast.js
+++ client/src/containers/Broadcast/Broadcast.js
@@ -1,15 +1,43 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Button from '../../components/Buttons/Button'
 import Navbar from '../../components/Navbar/Navbar'
+import Modal from 'react-modal'
+import './Broadcast.css'
+
+Modal.defaultStyles.overlay.backgroundColor = 'rgba(45, 45, 47, 0.75)'
 
 function Broadcast() {
+  const [isModalOpen, setisModalOpen] = useState(false)
+
+  const closeModal = () => {
+    setisModalOpen(false)
+  }
+
+  const afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+    console.log('after opening modal')
+  }
+
+  const openModal = () => {
+    setisModalOpen(true)
+  }
+
   return (
     <>
       <Navbar />
       <div className='dashboard-container'>
         <h2 style={{ marginTop: '2rem' }}>Broadcasts</h2>
-        <Button title='Create new Broadcast' />
+        <Button fx={openModal} title='Create new Broadcast' />
       </div>
+      <Modal
+        className='broadcast-modal'
+        isOpen={isModalOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel='Example Modal'
+      >
+        <p>Some random text</p>
+      </Modal>
     </>
   )
 }
