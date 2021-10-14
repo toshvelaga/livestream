4426902895c994b0a70ebab38f42c88b2390dd78
Tosh Velaga <toshvelaga@gmail.com>
2021-10-14 16:44:09 -0600

    move google oauth into broadcast

--- client/src/containers/Broadcast/Broadcast.js
+++ client/src/containers/Broadcast/Broadcast.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { YOUTUBE_PRIVACY_POLICY } from '../../constants/constants'
+import {
+  YOUTUBE_PRIVACY_POLICY,
+  SCOPE,
+  DISCOVERY,
+} from '../../constants/constants'
 import Button from '../../components/Buttons/Button'
 import Selected from '../../components/Selected/Selected'
 import TextInput from '../../components/TextInput/TextInput'
@@ -11,11 +15,15 @@ import './Broadcast.css'
 Modal.defaultStyles.overlay.backgroundColor = 'rgba(45, 45, 47, 0.75)'
 Modal.defaultStyles.overlay.zIndex = 101
 
+/* global gapi */
+
 function Broadcast() {
   const [isModalOpen, setisModalOpen] = useState(false)
   const [youtubeTitle, setyoutubeTitle] = useState('')
   const [youtubeDescription, setyoutubeDescription] = useState('')
   const [youtubePrivacy, setyoutubePrivacy] = useState('')
+
+  let GoogleAuth
 
   const closeModal = () => {
     setisModalOpen(false)
@@ -34,6 +42,54 @@ function Broadcast() {
 
   const submitHandler = () => {
     alert('This was clicked.')
+  }
+
+  useEffect(() => {
+    handleClientLoad()
+  }, [])
+
+  function handleClientLoad() {
+    // Load the API's client and auth2 modules.
+    // Call the initClient function after the modules load.
+    gapi.load('client:auth2', initClient)
+  }
+
+  function initClient() {
+    // Initialize the gapi.client object, which app uses to make API requests.
+    // Get API key and client ID from API Console.
+    // 'scope' field specifies space-delimited list of access scopes.
+    gapi.client
+      .init({
+        apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        discoveryDocs: [DISCOVERY],
+        scope: SCOPE,
+      })
+      .then(function () {
+        GoogleAuth = gapi.auth2.getAuthInstance()
+
+        // Listen for sign-in state changes.
+        GoogleAuth.isSignedIn.listen(updateSigninStatus)
+
+        // Handle initial sign-in state. (Determine if user is already signed in.)
+        var user = GoogleAuth.currentUser.get()
+        setSigninStatus()
+      })
+  }
+
+  function setSigninStatus() {
+    var user = GoogleAuth.currentUser.get()
+    console.log(user)
+    var isAuthorized = user.hasGrantedScopes(SCOPE)
+    if (isAuthorized) {
+      console.log('signed in and authorized')
+    } else {
+      console.log('not authorized')
+    }
+  }
+
+  function updateSigninStatus() {
+    setSigninStatus()
   }
 
   return (
