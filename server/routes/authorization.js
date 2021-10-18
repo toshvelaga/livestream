1537d167a270d8886436097990f4c263e0b4eb3f
Tosh Velaga <toshvelaga@gmail.com>
2021-10-18 15:47:28 -0600

    add endpoint for twitch auth access token

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -1,10 +1,19 @@
 const express = require('express'),
   router = express.Router(),
-  pool = require('../db')
+  pool = require('../db'),
+  { default: axios } = require('axios')
+require('dotenv').config()
 
-router.post('/api/user/register', async (req, res) => {
-  return res.send({ msg: 'Authorize via Twitch' })
-  // console.log('hey mohsin')
+router.post('/api/authorize/twitch', async (req, res) => {
+  let authorizationCode = req.body.authorizationCode
+  return axios
+    .post(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET}&code=${authorizationCode}&grant_type=authorization_code&redirect_uri=${process.env.TWITCH_REDIRECT_URL}`
+    )
+    .then((res) => console.log(res))
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
 module.exports = router
