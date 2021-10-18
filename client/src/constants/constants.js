2ad3beb0c0ccbf6b36536a54c5133a291f2fc75f
Tosh Velaga <toshvelaga@gmail.com>
2021-10-18 14:12:43 -0600

    edit constants

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,11 +1,9 @@
 export const SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl'
-
 export const TWITCH_SCOPE = encodeURIComponent(
   'channel:manage:broadcast channel:read:stream_key'
 )
 
 export const TWITCH_REDIRECT_URL = 'http://localhost:3000/destinations'
-
 export const TWITCH_RESPONSE_TYPE = 'token'
 
 export const DISCOVERY =
