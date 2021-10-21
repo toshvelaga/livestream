a61c7b9947abe2ce28771b66e0b1ef4ef8816530
Tosh Velaga <toshvelaga@gmail.com>
2021-10-21 01:21:39 -0600

    style Card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import './Card.css'
+// import * as FaIcons from 'react-icons/fa'
 
 function Card(props) {
   return (
     <div onClick={props.onClick} className='card-styles'>
       <div className='inner-card-content'>
         {props.children}
-        <p
-          style={{
-            marginTop: 0,
-            paddingTop: 0,
-            fontSize: '20px',
-            color: 'darkgrey',
-          }}
-        >
-          {props.title}
-        </p>
+        <p className='card-title'>{props.title}</p>
+        {/* <FaIcons.FaCheckCircle size='20' color='green' /> */}
       </div>
     </div>
   )
