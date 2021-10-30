5d04f55d5caeca86969cce9129b691c4fffb2ca4
Tosh Velaga <toshvelaga@gmail.com>
2021-10-29 19:44:07 -0600

    edit twitchBroadcast

--- server/routes/twitchBroadcast.js
+++ server/routes/twitchBroadcast.js
@@ -5,6 +5,7 @@ const express = require('express'),
 require('dotenv').config()
 
 router.patch('/api/twitch/broadcast', async (req, res) => {
+  const userId = req.body.userId
   const twitchUserID = req.body.twitchUserID
   const twitchAccessToken = req.body.twitchAccessToken
   const twitchAccessRefreshToken = req.body.twitchAccessRefreshToken
@@ -26,12 +27,30 @@ router.patch('/api/twitch/broadcast', async (req, res) => {
       console.log(res)
       return res
     })
-    .catch(async (err) => {
+    .catch(async (err, twitchUserID, title) => {
       console.log(err.response.status)
       if (err.response.status === 401) {
         // return refreshTwitchToken(twitchAccessRefreshToken)
         let data = await refreshTwitchToken(twitchAccessRefreshToken)
         console.log(data)
+        const { access_token, refresh_token } = data
+        // add access_token and refresh_token to DB
+        // await updateDbTwitchValues(userId, access_token, refresh_token)
+        console.log(access_token)
+
+        // setTimeout(() => {
+        //   axios.patch(
+        //     `https://api.twitch.tv/helix/channels?broadcaster_id=${twitchUserID}`,
+        //     { title: 'hello' },
+        //     {
+        //       headers: {
+        //         Authorization: `Bearer ${access_token}`,
+        //         'Client-Id': process.env.TWITCH_CLIENT_ID,
+        //         'Content-Type': 'application/json',
+        //       },
+        //     }
+        //   )
+        // }, 5000)
       }
     })
 
