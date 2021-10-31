2bb4c2dc0de977d9fbd4b7a6dc48f64483680fb5
Tosh Velaga <toshvelaga@gmail.com>
2021-10-31 01:15:48 -0600

    facebook long token

--- server/routes/facebookAuthorization.js
+++ server/routes/facebookAuthorization.js
@@ -6,7 +6,13 @@ require('dotenv').config()
 
 router.post('/api/authorize/facebook', async (req, res) => {
   // fB docs: https://developers.facebook.com/docs/pages/access-tokens/
+  const { facebookAccessToken } = req.body
 
+  const data = await axios.get(
+    `https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=${process.env.FACEBOOK_APP_ID}&client_secret=${process.env.FACEBOOK_APP_SECRET}&fb_exchange_token=${facebookAccessToken}`
+  )
+
+  console.log(data)
   console.log('get long facebook token')
 })
 
