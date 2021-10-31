871c40fb6f1560f413728eea7f74334cf87e5ce9
Tosh Velaga <toshvelaga@gmail.com>
2021-10-31 01:59:13 -0600

    updateDbFacebookValues to facebookAuthorization

--- server/routes/facebookAuthorization.js
+++ server/routes/facebookAuthorization.js
@@ -1,3 +1,5 @@
+const updateDbFacebookValues = require('../utils/updateDbFacebookValues')
+
 const express = require('express'),
   router = express.Router(),
   pool = require('../db'),
@@ -6,15 +8,17 @@ require('dotenv').config()
 
 router.post('/api/authorize/facebook', async (req, res) => {
   // fB docs: https://developers.facebook.com/docs/pages/access-tokens/
+  const { userId } = req.body
   const { facebookAccessToken } = req.body
 
   const result = await axios.get(
     `https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=${process.env.FACEBOOK_APP_ID}&client_secret=${process.env.FACEBOOK_APP_SECRET}&fb_exchange_token=${facebookAccessToken}`
   )
 
-  console.log(result.data.access_token)
+  console.log('get long FB access token ' + result.data.access_token)
   const longFacebookAccessToken = result.data.access_token
-  console.log('get long facebook token')
+
+  updateDbFacebookValues(userId, longFacebookAccessToken)
   return res
     .status(200)
     .send({ longFacebookAccessToken: longFacebookAccessToken })
