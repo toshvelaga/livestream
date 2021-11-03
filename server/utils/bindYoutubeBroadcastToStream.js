8ae84b3df2780e4920fd56feafd4aeac0574c85e
Tosh Velaga <toshvelaga@gmail.com>
2021-11-02 20:24:26 -0600

    bindYoutubeBroadcastToStream function

--- server/utils/bindYoutubeBroadcastToStream.js
+++ server/utils/bindYoutubeBroadcastToStream.js
@@ -1,8 +1,26 @@
 const { default: axios } = require('axios')
 require('dotenv').config()
 
-const bindYoutubeBroadcastToStream = async (youtubeAccessToken) => {
-  return { msg: 'this is it' }
+const bindYoutubeBroadcastToStream = async (
+  youtubeBroadcastId,
+  youtubeStreamId,
+  youtubeAccessToken
+) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${youtubeAccessToken}`,
+      Accept: 'application/json',
+    },
+  }
+
+  const bindedBroadcast = await axios.post(
+    `https://youtube.googleapis.com/youtube/v3/liveBroadcasts/bind?id=${youtubeBroadcastId}&part=id%2Csnippet%2CcontentDetails%2Cstatus&streamId=${youtubeStreamId}&key=${process.env.GOOGLE_API_KEY}`,
+    config
+  )
+
+  console.log(bindedBroadcast)
+
+  return bindedBroadcast
 }
 
 module.exports = bindYoutubeBroadcastToStream
