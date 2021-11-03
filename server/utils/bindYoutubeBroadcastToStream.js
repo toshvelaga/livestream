dabee8b8bc159e883dcf912e1ef8f8d15422d498
Tosh Velaga <toshvelaga@gmail.com>
2021-11-02 20:04:18 -0600

    add bindYoutubeBroadcastToStream

---
+++ server/utils/bindYoutubeBroadcastToStream.js
@@ -0,0 +1,8 @@
+const { default: axios } = require('axios')
+require('dotenv').config()
+
+const bindYoutubeBroadcastToStream = async (youtubeAccessToken) => {
+  return { msg: 'this is it' }
+}
+
+module.exports = bindYoutubeBroadcastToStream
