8239018a7a03f19a9117bf72ed801fb0d5e8c7aa
Tosh Velaga <toshvelaga@gmail.com>
2021-12-18 23:35:36 -0700

    edit ffmpeg with tee

--- server/server.js
+++ server/server.js
@@ -66,15 +66,54 @@ wss.on('connection', (ws, req) => {
   const youtube = myURL.searchParams.get('youtubeUrl')
   const facebook = myURL.searchParams.get('facebookUrl')
 
-  const ffmpegInput = inputSettings.concat(
-    twitchSettings(twitch),
-    youtubeSettings(youtube),
-    facebookSettings(facebook)
-  )
+  // const ffmpegInput = inputSettings.concat(
+  //   twitchSettings(twitch),
+  //   youtubeSettings(youtube),
+  //   facebookSettings(facebook)
+  // )
 
-  console.log(ffmpegInput)
+  const ffmpeg = child_process.spawn('ffmpeg', [
+    '-i',
+    '-',
+    '-map',
+    '0',
+    // video codec config: low latency, adaptive bitrate
+    '-c:v',
+    'libx264',
+    '-preset',
+    'veryfast',
+    '-tune',
+    'zerolatency',
+    '-g:v',
+    '60',
 
-  const ffmpeg = child_process.spawn('ffmpeg', ffmpegInput)
+    // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+    '-c:a',
+    'aac',
+    '-strict',
+    '-2',
+    '-ar',
+    '44100',
+    '-b:a',
+    '64k',
+
+    //force to overwrite
+    '-y',
+
+    // used for audio sync
+    '-use_wallclock_as_timestamps',
+    '1',
+    '-async',
+    '1',
+
+    '-f',
+    'flv',
+    '-flags',
+    '+global_header',
+    '-f',
+    'tee',
+    `[f=flv:onfail=ignore]${facebook}|[f=flv:onfail=ignore]${youtube}`,
+  ])
 
   // If FFmpeg stops for any reason, close the WebSocket connection.
   ffmpeg.on('close', (code, signal) => {
