f3671ca36153cfbbe0344bdad1b369ae224b15bf
Tosh Velaga <toshvelaga@gmail.com>
2022-01-18 17:46:17 -0700

    settext in settings

--- client/src/containers/Settings/Settings.js
+++ client/src/containers/Settings/Settings.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import TextArea from '../../components/TextArea/TextArea'
+import getCookie from '../../utils/getCookie'
 import './Settings.css'
 
 function Settings() {
+  const [email, setemail] = useState('')
+  const [text, settext] = useState('')
+
   useEffect(() => {
-    console.log('you user email')
+    setemail(getCookie('userEmail'))
   }, [])
   return (
     <>
@@ -14,11 +19,17 @@ function Settings() {
         <p>
           You are currently on the <b>FREE TIER</b>
         </p>
-        <p>User email: </p>
         <p>
-          Have a question about your account? Send us an email using the form
-          below.
+          Your email: <b>{email}</b>
         </p>
+        <br></br>
+        <div style={{ width: '80%' }}>
+          <TextArea
+            label='Have a question about your account? Send us an email here.'
+            value={text}
+            onChange={(e) => settext(e.target.value)}
+          />
+        </div>
       </div>
     </>
   )
