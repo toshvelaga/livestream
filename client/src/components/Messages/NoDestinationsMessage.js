9f637906c73074e4d7d55af7e0122dbbe6b44c19
Tosh Velaga <toshvelaga@gmail.com>
2022-02-15 01:45:07 -0700

    add more destinations button to view

--- client/src/components/Messages/NoDestinationsMessage.js
+++ client/src/components/Messages/NoDestinationsMessage.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import styles from '../../styles/styles'
 import { Link } from 'react-router-dom'
+import './NoDestinationsMessage.css'
 
 const NoDestinationsMessage = () => {
   return (
-    <p style={{ color: styles.greyFontColor, marginTop: 0 }}>
-      Please add at least one{' '}
-      <Link style={{ color: styles.blueFontColor }} to='/destinations'>
+    <>
+      <p
+        style={{
+          color: styles.greyFontColor,
+          marginTop: 0,
+        }}
+      >
+        Connect YouTube, Twitch, and FB:
+        {/* <Link style={{ color: styles.blueFontColor }} to='/destinations'>
         destination
-      </Link>{' '}
-      to go live.
-    </p>
+      </Link>{' '} */}
+        <Link style={{ marginLeft: '1rem' }} to='/destinations'>
+          <button className='connect-button'>Connect More Destinations</button>
+        </Link>
+      </p>
+    </>
   )
 }
 
