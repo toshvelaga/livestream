7e22e45d480d5248e2b0591b2772830177c872e6
Tosh Velaga <toshvelaga@gmail.com>
2022-02-16 18:30:43 -0700

    edit hubspot utils

--- server/utils/hubspot.js
+++ server/utils/hubspot.js
@@ -2,7 +2,31 @@ const { default: axios } = require('axios')
 require('dotenv').config()
 
 const hubspot = async () => {
-  console.log('hubspot')
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  const hb = await axios
+    .post(
+      `https://api.hubapi.com/contacts/v1/contact/`,
+      {
+        params: {
+          hapikey: 'demo',
+        },
+      },
+      config
+    )
+    .then((res) => {
+      console.log(res)
+      return res
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  return hb
 }
 
 module.exports = hubspot
