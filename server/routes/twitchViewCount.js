f7944b3268c68b6608f59dcd7dd2bc6e343a4d8e
Tosh Velaga <toshvelaga@gmail.com>
2022-03-01 19:43:08 -0700

    twitchViewCount api edited

--- server/routes/twitchViewCount.js
+++ server/routes/twitchViewCount.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-  router = express.Router()
+  router = express.Router(),
+  { default: axios } = require('axios')
 
 require('dotenv').config()
 
@@ -9,8 +10,8 @@ router.post('/api/twitch/view-count', async (req, res) => {
   const twitchUserName = req.body.twitchUserName
   const twitchAccessToken = req.body.twitchAccessToken
 
-  let authData = await axios
-    .get(`https://api.twitch.tv/helix/streams?user_login=toshvelaga`, {
+  let resData = await axios
+    .get(`https://api.twitch.tv/helix/streams?user_login=${twitchUserName}`, {
       headers: {
         Authorization: `Bearer ${twitchAccessToken}`,
         'Client-Id': process.env.TWITCH_CLIENT_ID,
@@ -24,14 +25,13 @@ router.post('/api/twitch/view-count', async (req, res) => {
     .catch((err) => {
       console.log(err.response.status)
       if (err.response.status === 401) {
-        // return refreshTwitchToken(twitchAccessRefreshToken)
         console.log('the wrong token was used, thats why there is a 401')
       }
     })
 
-  console.log(authData)
+  console.log(resData)
 
-  return res.status(201).send({ viewCount: 'send view count' })
+  return res.status(201).send(resData.data)
 })
 
 module.exports = router
