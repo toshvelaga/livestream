bf2d39ae788b89bccf0c1e3aca8b0ba736ea66e5
Tosh Velaga <toshvelaga@gmail.com>
2022-03-02 21:18:55 -0700

    css for view counter

--- client/src/components/ViewCounter/ViewCounter.js
+++ client/src/components/ViewCounter/ViewCounter.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './ViewCounter.css'
 
-const ViewCounter = () => {
-  return <div>ViewCounter</div>
+const ViewCounter = (num) => {
+  return <div className='view-counter'>Views: {num}</div>
 }
 
 export default ViewCounter
