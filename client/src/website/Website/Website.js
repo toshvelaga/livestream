ffa4e9c000fa02e4c1116ba193eb0df3c7f3d86d
Tosh Velaga <toshvelaga@gmail.com>
2022-03-24 18:28:48 -0700

    change width of create broadcast button

--- client/src/website/Website/Website.js
+++ client/src/website/Website/Website.js
@@ -191,6 +191,7 @@ function Website(props) {
           <PricingPlan />
         </div>
         <div
+          className='iframe-container'
           style={{
             display: 'flex',
             justifyContent: 'center',
