9ccea9eb64026c7f4f613a6cb954fef8245bded3
Tosh Velaga <toshvelaga@gmail.com>
2022-04-20 19:21:03 -0600

    fix page could not be found

--- client/src/containers/PageNotFound/PageNotFound.js
+++ client/src/containers/PageNotFound/PageNotFound.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './PageNotFound.css'
+import NoPageFound404 from '../../assets/404.png'
 
 function PageNotFound() {
+  console.log('Page Not Found')
   return (
-    <div>
+    <div className='page-not-found-container'>
+      <h1>404</h1>
       <h2 className='page-not-found-text'>
         Whoops...sorry friend this page was not found!
       </h2>
